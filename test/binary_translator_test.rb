9cf0daf0e7677618d244a4c897a3090c382cd2bf
Harper Bellows <harper.bellows@gmail.com>
2018-07-05 13:44:55 -0600

    add testing for single letter and whole word translation functionality

---
+++ test/binary_translator_test.rb
@@ -0,0 +1,44 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/binary_translator'
+
+class BinaryTranslatorTest < Minitest::Test
+  def test_it_exists
+    bt = BinaryTranslator.new
+
+    assert_instance_of BinaryTranslator, bt
+  end
+
+  def test_it_can_translate_the_a_single_letter
+    bt = BinaryTranslator.new
+
+    assert_equal "000001", bt.translate("a")
+    assert_equal "011010", bt.translate("z")
+  end
+
+  def test_it_can_transalte_a_word
+    bt = BinaryTranslator.new
+
+    result = "010100010101010010001001001110000111"
+
+    assert_equal result, bt.translate("turing")
+  end
+
+  def test_it_can_translate_capital_letters
+    bt = BinaryTranslator.new
+
+    assert_equal "000001", bt.translate("a")
+    assert_equal "000001", bt.translate("A")
+
+    result = "010100010101010010001001001110000111"
+
+    assert_equal result, bt.translate("TURING")
+  end
+
+  def test_it_can_translate_a_space
+    bt = BinaryTranslator.new
+
+    assert_equal "000000", bt.translate(" ")
+  end
+
+end
